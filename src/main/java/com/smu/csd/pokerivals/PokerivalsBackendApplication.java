8aa8bde644d97b56dc6bbb1a941ac4686fec3836
jsamaze <111722374+jsamaze@users.noreply.github.com>
2024-10-04 09:11:38 +0800

    Spring Initializr

---
+++ src/main/java/com/smu/csd/pokerivals/PokerivalsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smu.csd.pokerivals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerivalsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerivalsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smu/csd/pokerivals/PokerivalsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smu.csd.pokerivals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerivalsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
