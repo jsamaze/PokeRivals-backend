edda9e6cebd54adb8c4862f5e81b7732a33223c1
MarcoKuok <marco.2022@scis.smu.edu.sg>
2024-10-16 17:50:32 +0800

    Added descriptions for the API endpoints for swagger-ui and other miscellaneous stuffs

--- src/main/java/com/smu/csd/pokerivals/controller/PlayerController.java
+++ src/main/java/com/smu/csd/pokerivals/controller/PlayerController.java
@@ -63,42 +63,105 @@ public class PlayerController {
     }
 
     @PostMapping("me/friend/{username}")
-    @Operation(summary = "add friend for the logged-in user and the given username")
+    @Operation(summary= "Add friend by username",
+            description = "Logged-in user can add friend by the username")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Added friend successfully.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Message.class)) }),
+            @ApiResponse(responseCode = "400", description = "Failed to add friend.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Message.class)) }),
+            @ApiResponse(responseCode = "403", description = "Forbidden access.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Message.class)) })})
     public Message addFriend(@PathVariable String username,@AuthenticationPrincipal UserDetails userDetails) throws UnsupportedEncodingException{
         playerService.connectAsFriends(userDetails.getUsername(),username);
-        return new Message("Become friends successfully");
+        return new Message("Added friend successfully");
     }
 
     @DeleteMapping("me/friend/{username}")
-    @Operation(summary = "remove friend for the logged-in user and the given username")
+    @Operation(summary= "Remove friend by given username",
+            description = "Logged-in user can remove friend by the username")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Removed friend successfully.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Message.class)) }),
+            @ApiResponse(responseCode = "400", description = "Failed to remove friend.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Message.class)) }),
+            @ApiResponse(responseCode = "403", description = "Forbidden access.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Message.class)) })})
     public Message removeFriend(@PathVariable String username,@AuthenticationPrincipal UserDetails userDetails) throws UnsupportedEncodingException{
         playerService.disconnectAsFriends(userDetails.getUsername(),username);
-        return new Message("Removed friends connectionsuccessfully");
+        return new Message("Removed friend successfully");
     }
 
     @GetMapping("me/friend")
-    @Operation(summary ="get my friends in a paged way")
+    @Operation(summary= "Get user's friends",
+            description = "Get the logged-in user's friends")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Get friends successfully.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = List.class)) }),
+            @ApiResponse(responseCode = "400", description = "Failed to get friends.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Message.class)) }),
+            @ApiResponse(responseCode = "403", description = "Forbidden access.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Message.class)) })})
     public List<Player> getFriends(@AuthenticationPrincipal UserDetails userDetails){
         return playerService.getFriendsOf(userDetails.getUsername());
     }
 
     @GetMapping("")
-    @Operation(summary ="get user by searching username")
-    public List<Player> getFriends(@AuthenticationPrincipal UserDetails userDetails, @RequestParam String query){
+    @Operation(summary= "Get users by username",
+            description = "Get users by searching for their username")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Get users successfully.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = List.class)) }),
+            @ApiResponse(responseCode = "400", description = "Failed to get users.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Message.class)) })})
+    public List<Player> getUsers(@AuthenticationPrincipal UserDetails userDetails, @RequestParam String query){
         return playerService.searchPlayers(query);
     }
 
     @GetMapping("{username}")
-    @Operation(summary = "get player details")
+    @Operation(summary= "Get player by username",
+            description = "Get player by searching for their username")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Get player successfully.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Player.class)) }),
+            @ApiResponse(responseCode = "400", description = "Failed to get player.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Message.class)) }),
+            @ApiResponse(responseCode = "403", description = "Forbidden access.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Message.class)) })})
     public Player getPlayer(@PathVariable String username) {
         return playerService.getUser(username);
     }
 
     @PatchMapping("me/clan/{clanName}")
-    @Operation(summary = "set my clan")
+    @Operation(summary= "Set my clan",
+            description = "Set my clan to the clan name")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Set clan successfully.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Message.class)) }),
+            @ApiResponse(responseCode = "400", description = "Failed to set clan.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Message.class)) }),
+            @ApiResponse(responseCode = "403", description = "Forbidden access.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Message.class)) })})
     public  Message setClan(@AuthenticationPrincipal UserDetails userDetails, @PathVariable String clanName){
         playerService.addToClan(userDetails.getUsername(),clanName);
-        return new Message("Added to clan successfully");
+        return new Message("Set clan successfully");
     }
 
 
